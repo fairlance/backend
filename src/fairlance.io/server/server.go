38597e9a2c65038564e0afbd64cd3ba6a3d68430
Miloš Krsmanović <milos.krsman@gmail.com>
2015-09-20 22:07:00 +0200

    Card(ASYFzFty) Simple registration implementation using MongoDB

--- src/fairlance.io/server/server.go
+++ src/fairlance.io/server/server.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	"html"
+	"gopkg.in/mgo.v2"
 	"log"
 	"net/http"
 )
@@ -13,16 +13,52 @@ type RegisteredUser struct {
 	Email string
 }
 
+func Register(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	if name != "" && email != "" {
+		session, err := mgo.Dial("localhost")
+		if err != nil {
+			fmt.Printf("Can't connect to mongo, go error %v\n", err)
+			panic(err)
+		}
+		defer session.Close()
+		c := session.DB("registration").C("people")
+		err = c.Insert(&RegisteredUser{name, email})
+		if err != nil {
+			fmt.Printf("Can't save to mongo, go error %v\n", err)
+			panic(err)
+		} else {
+			fmt.Fprintf(w, "Registered user %q with email %q", name, email)
+		}
+	}
+
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	var results []RegisteredUser
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		fmt.Printf("Can't connect to mongo, go error %v\n", err)
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB("registration").C("people")
+	err = c.Find(nil).All(&results)
+	if err != nil {
+		fmt.Printf("Can't query mongo, go error %v\n", err)
+		panic(err)
+	}
+	fmt.Fprintf(w, "Results All: %q", results)
 }
 
 func main() {
-
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	router.HandleFunc("/", Index).Methods("GET")
+	router.HandleFunc("/register", Register).Methods("POST")
 
 	fmt.Println("Starting sever on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", router))
-
 }
