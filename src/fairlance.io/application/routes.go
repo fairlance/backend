dd4799269f0619847f77c4bc6af12f11b0b6ae16
Miloš Krsmanović <milos.krsman@gmail.com>
2016-03-08 20:53:22 +0100

    Make /freelancer open to access

--- src/fairlance.io/application/routes.go
+++ src/fairlance.io/application/routes.go
@@ -16,7 +16,7 @@ type Routes []Route
 var routes = Routes{
     Route{"Login", "POST", "/login", http.HandlerFunc(Login)},
     Route{"Index", "GET", "/", http.HandlerFunc(Index)},
-    Route{"IndexFreelancer", "GET", "/freelancer/", AuthHandler(http.HandlerFunc(IndexFreelancer))},
+    Route{"IndexFreelancer", "GET", "/freelancer/", http.HandlerFunc(IndexFreelancer)},
     Route{"NewFreelancer", "POST", "/freelancer/new", http.HandlerFunc(NewFreelancer)},
     Route{"GetFreelancer", "GET", "/freelancer/{id}", AuthHandler(http.HandlerFunc(GetFreelancer))},
     Route{"DeleteFreelancer", "DELETE", "/freelancer/{id}", AuthHandler(http.HandlerFunc(DeleteFreelancer))},
