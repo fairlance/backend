4357f68ef493edfd00ca19bdc0dcb365133212c5
Miloš Krsmanović <milos.krsman@gmail.com>
2016-06-11 23:26:17 +0200

    Use parameter for port

--- src/fairlance.io/application/main.go
+++ src/fairlance.io/application/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"flag"
 	"net/http"
+	"strconv"
 )
 
+var port int
+
 func main() {
+	flag.IntVar(&port, "port", 3001, "Specify the port to listen to.")
+	flag.Parse()
+
 	var appContext, err = NewContext("application")
 	appContext.DropCreateFillTables()
 	if err != nil {
@@ -16,4 +23,5 @@ func main() {
 	if err := http.ListenAndServe(":3001", nil); err != nil {
 		panic(err)
 	}
+	panic(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
