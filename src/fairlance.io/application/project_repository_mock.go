b8e838ed848b9511d714be481fc5bcf373262613
Miloš Krsmanovic <milos.krsman@gmail.com>
2016-12-26 21:44:22 +0100

    [application] Fix build
    (cherry picked from commit 69e0cfa)

--- src/fairlance.io/application/project_repository_mock.go
+++ src/fairlance.io/application/project_repository_mock.go
@@ -16,6 +16,14 @@ type ProjectRepositoryMock struct {
 			Error   error
 		}
 	}
+	AddCall struct {
+		Receives struct {
+			Project *Project
+		}
+		Returns struct {
+			Error error
+		}
+	}
 }
 
 func (repo *ProjectRepositoryMock) GetAllProjects() ([]Project, error) {
@@ -27,4 +35,9 @@ func (repo *ProjectRepositoryMock) GetByID(id uint) (Project, error) {
 	repo.GetByIDCall.Receives.ID = id
 	return repo.GetByIDCall.Returns.Project,
 		repo.GetByIDCall.Returns.Error
+}
+
+func (repo *ProjectRepositoryMock) Add(project *Project) error {
+	repo.AddCall.Receives.Project = project
+	return repo.AddCall.Returns.Error
 }
