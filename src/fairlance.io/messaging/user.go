c90626c68fdd41387cec4b86b5294ce4a4e02711
Miloš Krsmanović <milos.krsman@gmail.com>
2017-04-20 22:27:20 +0200

    [messaging] socket open for 4 hours

--- src/fairlance.io/messaging/user.go
+++ src/fairlance.io/messaging/user.go
@@ -15,7 +15,7 @@ const (
 	writeWait = 10 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
-	readWait = 8 * time.Hour
+	readWait = 4 * time.Hour
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 2048
