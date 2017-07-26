b42a0cb47189c9e166eebb924015e3030c7264ae
Milos Krsmanovic <milos.krsman@gmail.com>
2017-07-26 23:25:04 +0200

    [fileserver] Limit files to 3MB, generate a random name after upload

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"strings"
 
 	"github.com/fairlance/backend/middleware"
+	"github.com/google/uuid"
 	respond "gopkg.in/matryer/respond.v1"
 )
+
+const _3MB = 3 * 1024 * 1024
 
 func main() {
 	log.SetFlags(log.Lshortfile)
@@ -20,12 +25,12 @@ func main() {
 	if err := os.MkdirAll(dataDir, 0755); err != nil {
 		log.Fatalf("error creating folder: %v", err)
 	}
-	http.Handle("/file/", middleware.Chain(
+	http.Handle("/public/file/", middleware.Chain(
 		middleware.CORSHandler,
 		middleware.JSONEnvelope,
 		middleware.HTTPMethod("GET"),
-	)(http.StripPrefix("/file/", http.FileServer(http.Dir(dataDir)))))
-	http.Handle("/upload", middleware.Chain(
+	)(http.StripPrefix("/public/file/", http.FileServer(http.Dir(dataDir)))))
+	http.Handle("/public/upload", middleware.Chain(
 		middleware.CORSHandler,
 		middleware.JSONEnvelope,
 		middleware.WithTokenFromHeader,
@@ -38,86 +43,65 @@ func main() {
 
 func upload(dataDir string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			respond.With(w, r, http.StatusMethodNotAllowed, errors.New("bad method, only POST is allowed"))
-			return
-		}
-
-		r.ParseMultipartForm(32 << 20)
+		r.Body = http.MaxBytesReader(w, r.Body, _3MB)
 		file, header, err := r.FormFile("uploadfile")
 		if err != nil {
-			log.Println(err)
-			respond.With(w, r, http.StatusBadRequest, err)
+			log.Println(fmt.Errorf("could not parse form: %v", err))
+			respond.With(w, r, http.StatusBadRequest, fmt.Errorf("could not parse form: %v", err))
 			return
 		}
 		defer file.Close()
-
 		fileStartBuff := make([]byte, 512)
 		read, err := file.Read(fileStartBuff)
 		if err != nil {
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("could not read bytes from file: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("could not read bytes from file: %v", err))
 			return
 		}
-
 		if read == 0 {
-			err = errors.New("0 bytes were read for content type detecting")
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("read 0 bytes from file: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("read 0 bytes from file: %v", err))
 			return
 		}
-
 		fileType := http.DetectContentType(fileStartBuff)
-
 		file.Seek(0, 0)
+		uuid, err := uuid.NewUUID()
+		ext := filepath.Ext(header.Filename)
+		if ext == "" {
+			ext = "." + strings.Split(fileType, "/")[1]
+		}
+		name := uuid.String() + ext
 		if err != nil {
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("could not generate uuid: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("could not generate uuid: %v", err))
 			return
 		}
-
-		f, err := os.Create(dataDir + "/" + header.Filename)
+		f, err := os.Create(dataDir + "/" + name)
 		if err != nil {
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("could not create destination file: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("could not create destination file: %v", err))
 			return
 		}
 		defer f.Close()
-
 		written, err := io.Copy(f, file)
 		if err != nil {
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("could not copy file to destination file: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("could not copy file to destination file: %v", err))
 			return
 		}
-
 		if written == 0 {
-			err = errors.New("0 bytes were written")
-			log.Println(err)
-			respond.With(w, r, http.StatusInternalServerError, err)
+			log.Println(fmt.Errorf("wrote 0 bytes to destination file: %v", err))
+			respond.With(w, r, http.StatusInternalServerError, fmt.Errorf("wrote 0 bytes to destination file: %v", err))
 			return
 		}
-
 		respond.With(w, r, http.StatusOK, struct {
 			Name string `json:"name"`
 			URL  string `json:"url"`
 			Type string `json:"type"`
 		}{
-			Name: header.Filename,
-			URL:  "file/" + header.Filename,
+			Name: name,
+			URL:  "file/" + name,
 			Type: fileType,
 		})
 	})
 }
-
-// func index() http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Add("Content-Type", "text/html")
-// 		w.Write([]byte(`
-//         <form enctype="multipart/form-data" action="/upload" method="post">
-//             <input type="file" name="uploadfile" />
-//             <input type="submit" value="upload" />
-//         </form>
-//         `))
-// 	})
-// }
